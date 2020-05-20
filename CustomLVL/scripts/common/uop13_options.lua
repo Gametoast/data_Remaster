3884843b8c8e35748f4825a083a7f9d9af4467e4
Anakin <Battlefrontler@t-online.de>
2020-05-20 17:16:00 +0200

    remove award effect for campaign and gc
    Former-commit-id: 6a7c2443365ceab992f2762eb90a27d953fc448b

--- CustomLVL/scripts/common/uop13_options.lua
+++ CustomLVL/scripts/common/uop13_options.lua
@@ -7,7 +7,7 @@ if ScriptCB_IsFileExist then
 	
 	-- backup old function
 	local remaV13_ScriptCB_IsFileExist = ScriptCB_IsFileExist
-	
+
 	-- wrap ScriptCB_IsFileExist
 	ScriptCB_IsFileExist = function(...)
 	
@@ -24,17 +24,8 @@ if ScriptCB_IsFileExist then
 			end
 			
 		elseif arg[1] == "..\\..\\addon\\AAA-v1.3patch\\settings\\noAwards.txt" then
-
-			if not rema_database then
-				print("Houston, we got a problem!!")
-			end
-			
-			if rema_database.data.awardEffects == 2 then
-				return 0
-			else
-				return 1
-			end
-			
+			-- we are doing this somewhere else
+			return 0
 		else
 			return remaV13_ScriptCB_IsFileExist(unpack(arg))
 		end
@@ -43,4 +34,29 @@ if ScriptCB_IsFileExist then
 else
 	print("Remaster: Error")
 	print("        : ScriptCB_IsFileExist() not found!")
+end
+
+
+-- try to wrap ScriptPostLoad ------------------------------------
+if ScriptPostLoad then
+	-- backup old function
+	local remaV13_ScriptPostLoad = ScriptPostLoad
+
+	-- wrap ScriptPostLoad
+
+	ScriptPostLoad = function(...)
+
+		if not rema_database then
+			print("Houston, we got a problem!!")
+		end
+		
+		if rema_database.data.awardEffects == 1 and ff_awardEffectsOn == 1 then
+			ff_CommandRemoveAwardEffects()
+		end
+		
+		return remaV13_ScriptPostLoad(unpack(arg))
+	end
+else
+	print("Remaster: Error")
+	print("        : ScriptPostLoad() not found!")
 end
